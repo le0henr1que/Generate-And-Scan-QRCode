ab2e05c063d6e3972da7f34b130cefbeef9aa5b3
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-08-30 16:26:42 -0300

    create only generate scan

---
+++ src/QRCode/Generate/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import QRCode from 'react-qr-code'
+
+const Generate = () => {
+    return (
+        <QRCode value="This Is Id Product" />
+    )
+}
+
+export default Generate
